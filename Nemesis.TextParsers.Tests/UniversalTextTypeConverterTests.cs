1b3f28bd9fd122f5eca55a69ac790b0c120f4bf9
Michał Bryłka <michal.brylka@gmail.com>
2019-09-11 12:02:42 +0200

    Universal converter based on TextParsing lib

---
+++ Nemesis.TextParsers.Tests/UniversalTextTypeConverterTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using NUnit.Framework;
+using JetBrains.Annotations;
+
+namespace Nemesis.TextParsers.Tests
+{
+    [TestFixture]
+    class UniversalTextTypeConverterTests
+    {
+        private static IEnumerable<(ITextBasedObject, string)> Correct_Data() => new (ITextBasedObject, string)[]
+        {
+            (new CommaSeparated(3.14, 15), "3.14,15"),
+            (new CommaSeparated(-50.555555, 111111111), "-50.555555,111111111"),
+        };
+
+        [TestCaseSource(nameof(Correct_Data))]
+        public void CanTransformTextIntoObjects((ITextBasedObject instance, string text) data)
+        {
+            var converter = TypeDescriptor.GetConverter(data.instance.GetType());
+
+            var newText = converter.ConvertToInvariantString(data.instance);
+            var newInstance = converter.ConvertFromInvariantString(data.text);
+            var newText2 = converter.ConvertToInvariantString(newInstance ?? "");
+
+            Assert.That(newInstance, Is.EqualTo(data.instance));
+            Assert.That(newText, Is.EqualTo(data.text));
+            Assert.That(newText, Is.EqualTo(newText2));
+        }
+
+        [SuppressMessage("ReSharper", "UnusedMember.Global")]
+        [SuppressMessage("ReSharper", "UnusedMemberInSuper.Global")]
+        public interface ITextBasedObject
+        {
+            string ToText();
+            double Value1 { get; }
+            int Value2 { get; }
+        }
+
+        [TypeConverter(typeof(UniversalTextTypeConverter<CommaSeparated>))]
+        struct CommaSeparated : ITextBasedObject
+        {
+            public double Value1 { get; }
+            public int Value2 { get; }
+
+            public CommaSeparated(double value1, int value2)
+            {
+                Value1 = value1;
+                Value2 = value2;
+            }
+
+            public override string ToString() => FormattableString.Invariant($"{Value1},{Value2}");
+
+            [UsedImplicitly]
+            public static CommaSeparated FromText(string text) =>
+                text.Split(',') is var array && array.Length == 2
+                    ? new CommaSeparated(
+                        double.Parse(array[0], NumberStyles.Any, CultureInfo.InvariantCulture),
+                        int.Parse(array[1], NumberStyles.Any, CultureInfo.InvariantCulture)
+                        )
+                    : default;
+
+            public string ToText() => ToString();
+
+            private bool Equals(CommaSeparated other) => Value1.Equals(other.Value1) && Value2 == other.Value2;
+
+            public override bool Equals(object obj) => obj is CommaSeparated other && Equals(other);
+
+            public override int GetHashCode() => unchecked((Value1.GetHashCode() * 397) ^ Value2);
+        }
+
+    }
+}
---
+++ Nemesis.TextParsers/UniversalTextTypeConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+
+namespace Nemesis.TextParsers
+{
+    public sealed class UniversalTextTypeConverter<TValue> : TypeConverter
+    {
+        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType) =>
+            sourceType == typeof(string) || base.CanConvertFrom(context, sourceType);
+
+        public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType) =>
+            destinationType == typeof(string) || base.CanConvertTo(context, destinationType);
+
+
+        public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value) =>
+            value is string text
+                ? TextTransformer.Default.GetTransformer<TValue>().ParseFromText(text)
+                : base.ConvertFrom(context, culture, value);
+
+        public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType) =>
+            destinationType == typeof(string)
+                ? TextTransformer.Default.GetTransformer<TValue>().Format((TValue)value)
+                : base.ConvertTo(context, culture, value, destinationType);
+    }
+}
