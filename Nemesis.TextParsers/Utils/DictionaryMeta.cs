552e3fb33b3a57a6baf7ef7fa2a67346a0e1920a
Michał Bryłka <michal.brylka@gmail.com>
2020-03-16 10:27:32 +0100

    Reorganize folders

---
+++ Nemesis.TextParsers/Utils/DictionaryMeta.cs
@@ -0,0 +1,205 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Globalization;
+using System.Reflection;
+using System.Runtime.Serialization;
+using JetBrains.Annotations;
+using Nemesis.TextParsers.Runtime;
+
+namespace Nemesis.TextParsers
+{
+    /// <summary>
+    /// Aids in providing metadata for GUI applications 
+    /// </summary>
+    [PublicAPI]
+    public readonly struct DictionaryMeta : IEquatable<DictionaryMeta>
+    {
+        public DictionaryKind Kind { get; }
+        public Type KeyType { get; }
+        public Type ValueType { get; }
+
+        public bool IsValid => Kind != DictionaryKind.Unknown && KeyType != null && ValueType != null;
+
+        internal DictionaryMeta(DictionaryKind kind, Type keyType, Type valueType)
+        {
+            if (!Enum.IsDefined(typeof(DictionaryKind), kind))
+                throw new InvalidEnumArgumentException(nameof(kind), (int)kind, typeof(DictionaryKind));
+            Kind = kind;
+            KeyType = keyType;
+            ValueType = valueType;
+        }
+
+        public void Deconstruct(out DictionaryKind kind, out Type keyType, out Type valueType)
+        {
+            kind = Kind;
+            keyType = KeyType;
+            valueType = ValueType;
+        }
+
+        public override string ToString() => $"{Kind}<{KeyType?.GetFriendlyName() ?? "<NoType>"}, {ValueType?.GetFriendlyName() ?? "<NoType>"}>";
+
+        #region Equals
+        public bool Equals(DictionaryMeta other) => Kind == other.Kind && KeyType == other.KeyType && ValueType == other.ValueType;
+
+        public override bool Equals(object obj) => !(obj is null) && obj is DictionaryMeta other && Equals(other);
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = (int)Kind;
+                hashCode = (hashCode * 397) ^ (KeyType != null ? KeyType.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (ValueType != null ? ValueType.GetHashCode() : 0);
+                return hashCode;
+            }
+        }
+
+        public static bool operator ==(DictionaryMeta left, DictionaryMeta right) => left.Equals(right);
+
+        public static bool operator !=(DictionaryMeta left, DictionaryMeta right) => !left.Equals(right);
+        #endregion
+
+        public object CreateDictionary(IList<(object Key, object Value)> sourceElements)
+        {
+            if (!IsValid) return null;
+
+            var createDictMethod = typeof(DictionaryMeta).GetMethod(nameof(CreateDictionaryGeneric), BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static) ??
+                                   throw new MissingMethodException(nameof(DictionaryMeta), nameof(CreateDictionaryGeneric));
+            createDictMethod = createDictMethod.MakeGenericMethod(KeyType, ValueType);
+
+            return createDictMethod.Invoke(this, new object[] { sourceElements });
+        }
+
+        public IDictionary<TKey, TValue> CreateDictionaryGeneric<TKey, TValue>(IList<(object Key, object Value)> sourceElements)
+        {
+            if (!IsValid) return null;
+            int capacity = sourceElements?.Count ?? 0;
+
+            IDictionary<TKey, TValue> result =
+                Kind == DictionaryKind.SortedDictionary ? new SortedDictionary<TKey, TValue>() :
+                    (
+                        Kind == DictionaryKind.SortedList
+                            ? new SortedList<TKey, TValue>(capacity)
+                            : (IDictionary<TKey, TValue>)new Dictionary<TKey, TValue>(capacity)
+                    );
+
+            for (int i = 0; i < capacity; i++)
+            {
+                // ReSharper disable once PossibleNullReferenceException
+                var (oKey, oValue) = sourceElements[i];
+                TKey key = SpanParserHelper.ConvertElement<TKey>(oKey);
+                TValue value = SpanParserHelper.ConvertElement<TValue>(oValue);
+
+                if (key == null) key = GetQuiteUniqueNotNullKey();
+
+                result[key] = value;
+            }
+
+            return Kind == DictionaryKind.ReadOnlyDictionary
+                ? new ReadOnlyDictionary<TKey, TValue>(result)
+                : result;
+
+            TKey GetQuiteUniqueNotNullKey()
+            {
+                try
+                {
+                    var nextKey = capacity + 1;
+
+                    var typeOfKey = typeof(TKey);
+                    if (typeOfKey == typeof(string))
+                        return (TKey)(object)$"Key {nextKey}";
+                    else if (TypeMeta.IsNumeric(typeOfKey))
+                        return (TKey)Convert.ChangeType(nextKey % 128, typeOfKey, CultureInfo.InvariantCulture);
+                    else if (typeOfKey.IsValueType && Nullable.GetUnderlyingType(typeOfKey) is { } underlyingType)
+                        return (TKey)FormatterServices.GetUninitializedObject(underlyingType);
+                    else
+                        return (TKey)FormatterServices.GetUninitializedObject(typeOfKey);
+                }
+#pragma warning disable CA1031 // Do not catch general exception types
+                catch { return default; }
+#pragma warning restore CA1031 // Do not catch general exception types
+            }
+        }
+    }
+
+    internal static class DictionaryKindHelper
+    {
+        public static DictionaryMeta GetDictionaryMeta(Type dictType)
+        {
+            if (IsTypeSupported(dictType) &&
+                GetDictionaryKind(dictType) is DictionaryKind kind &&
+                kind != DictionaryKind.Unknown
+               )
+            {
+                var (keyType, valueType) = GetPairType(dictType);
+                return new DictionaryMeta(kind, keyType, valueType);
+            }
+
+            return default;
+        }
+
+        private static DictionaryKind GetDictionaryKind(Type dictType)
+        {
+            if (dictType.IsGenericType && dictType.GetGenericTypeDefinition() is { } definition)
+            {
+                if (definition == typeof(Dictionary<,>) ||
+                    definition == typeof(IDictionary<,>))
+                    return DictionaryKind.Dictionary;
+
+                else if (definition == typeof(ReadOnlyDictionary<,>) ||
+                         definition == typeof(IReadOnlyDictionary<,>))
+                    return DictionaryKind.ReadOnlyDictionary;
+
+                else if (definition == typeof(SortedList<,>))
+                    return DictionaryKind.SortedList;
+                else if (definition == typeof(SortedDictionary<,>))
+                    return DictionaryKind.SortedDictionary;
+            }
+
+            return DictionaryKind.Unknown;
+        }
+
+        private static (Type keyType, Type valueType) GetPairType(Type dictType)
+        {
+            var genType =
+                dictType.IsGenericType && dictType.GetGenericTypeDefinition() is { } definition &&
+                (definition == typeof(IDictionary<,>) || definition == typeof(IReadOnlyDictionary<,>))
+                    ? dictType
+                    : TypeMeta.GetConcreteInterfaceOfType(dictType, typeof(IDictionary<,>))
+                      ?? throw new InvalidOperationException("Type has to be or implement IDictionary<,> or IReadOnlyDictionary<,>");
+
+            return (genType.GenericTypeArguments[0], genType.GenericTypeArguments[1]);
+        }
+
+        private static readonly HashSet<Type> _supportedDictionaryTypes = new HashSet<Type>()
+        {
+            typeof(Dictionary<,>),
+            typeof(IDictionary<,>),
+
+            typeof(ReadOnlyDictionary<,>),
+            typeof(IReadOnlyDictionary<,>),
+
+            typeof(SortedList<,>),
+            typeof(SortedDictionary<,>),
+        };
+
+        public static bool IsTypeSupported(Type dictType) =>
+            dictType != null &&
+            dictType.IsGenericType &&
+            dictType.GetGenericTypeDefinition() is { } definition &&
+            _supportedDictionaryTypes.Contains(definition);
+    }
+
+    public enum DictionaryKind : byte
+    {
+        Unknown,
+        Dictionary,
+        SortedDictionary,
+        SortedList,
+        ReadOnlyDictionary
+    }
+}
+#nullable restore
---
+++ Nemesis.TextParsers/Utils/LeanCollection.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+
+namespace Nemesis.TextParsers
+{
+    [PublicAPI]
+    public static class LeanCollectionFactory
+    {
+        public static LeanCollection<T> FromArrayChecked<T>(T[] items) => new LeanCollection<T>(items);
+
+        public static LeanCollection<T> FromArray<T>(T[] items)
+        {
+            int? length = items?.Length;
+            switch (length)
+            {
+                case null:
+                case 0:
+                    return new LeanCollection<T>();
+                case 1:
+                    return new LeanCollection<T>(items[0]);
+                case 2:
+                    return new LeanCollection<T>(items[0], items[1]);
+                case 3:
+                    return new LeanCollection<T>(items[0], items[1], items[2]);
+                default:
+                    return new LeanCollection<T>(items);
+            }
+        }
+
+        public static LeanCollection<T> ToLeanCollection<T>(T one) => new LeanCollection<T>(one);
+        public static LeanCollection<T> ToLeanCollection<T>((T, T) pair) => new LeanCollection<T>(pair.Item1, pair.Item2);
+        public static LeanCollection<T> ToLeanCollection<T>((T, T, T) triple) => new LeanCollection<T>(triple.Item1, triple.Item2, triple.Item3);
+        public static LeanCollection<T> ToLeanCollection<T>(T[] span) => new LeanCollection<T>(span);
+
+        public static T ToSingleValue<T>(LeanCollection<T> one) => one._item1;
+        public static (T, T) ToPair<T>(LeanCollection<T> pair) => (pair._item1, pair._item2);
+        public static (T, T, T) ToTriple<T>(LeanCollection<T> triple) => (triple._item1, triple._item2, triple._item3);
+        public static T[] ToArray<T>(LeanCollection<T> more) => more._items;
+    }
+
+    /// <summary>
+    /// Stores up to 3 elements or an array
+    /// </summary>
+    public readonly struct LeanCollection<T> : IEquatable<LeanCollection<T>>
+    {
+        #region Fields
+        enum CollectionSize : sbyte { More = -1, [UsedImplicitly] Zero = 0, One = 1, Two = 2, Three = 3 }
+
+        private readonly CollectionSize _size;
+        public int Size => _size == CollectionSize.More ? _items.Length : (int)_size;
+
+        // ReSharper disable InconsistentNaming
+        internal readonly T _item1;
+        internal readonly T _item2;
+        internal readonly T _item3;
+        internal readonly T[] _items;
+        // ReSharper restore InconsistentNaming
+        #endregion
+
+        #region Constructors
+        public LeanCollection(T item1)
+        {
+            _item1 = item1;
+            _item2 = default;
+            _item3 = default;
+
+            _items = default;
+            _size = CollectionSize.One;
+        }
+
+        public LeanCollection(T item1, T item2)
+        {
+            _item1 = item1;
+            _item2 = item2;
+            _item3 = default;
+
+            _items = default;
+            _size = CollectionSize.Two;
+        }
+
+        public LeanCollection(T item1, T item2, T item3)
+        {
+            _item1 = item1;
+            _item2 = item2;
+            _item3 = item3;
+
+            _items = default;
+            _size = CollectionSize.Three;
+        }
+
+        internal LeanCollection(T[] items)
+        {
+            _item1 = default;
+            _item2 = default;
+            _item3 = default;
+
+            _size = CollectionSize.More;
+            _items = items;
+        }
+        
+        #endregion
+
+        #region Enumerations
+        public LeanCollectionEnumerator GetEnumerator() => new LeanCollectionEnumerator(this);
+
+        public ref struct LeanCollectionEnumerator
+        {
+            private readonly LeanCollection<T> _leanCollection;
+            private int _index;
+
+            public T Current { get; private set; }
+
+            public LeanCollectionEnumerator(in LeanCollection<T> leanCollection)
+            {
+                _leanCollection = leanCollection;
+                Current = default;
+                _index = leanCollection._size == CollectionSize.More ? 0 : -1;
+            }
+
+            public bool MoveNext()
+            {
+                var canMove = false;
+                if (_index >= 0)
+                {
+                    var items = _leanCollection._items;
+                    if (_index < items.Length)
+                    {
+                        Current = items[_index++];
+                        canMove = true;
+                    }
+                    else
+                        Current = default;
+                }
+                else
+                {
+                    if (_index == -1 && _leanCollection._size >= CollectionSize.One)
+                    {
+                        Current = _leanCollection._item1;
+                        canMove = true;
+                        --_index;
+                    }
+                    else if (_index == -2 && _leanCollection._size >= CollectionSize.Two)
+                    {
+                        Current = _leanCollection._item2;
+                        canMove = true;
+                        --_index;
+                    }
+                    else if (_index == -3 && _leanCollection._size >= CollectionSize.Three)
+                    {
+                        Current = _leanCollection._item3;
+                        canMove = true;
+                        --_index;
+                    }
+                    else
+                        Current = default;
+                }
+                return canMove;
+            }
+        }
+
+        /// <summary>
+        /// Convert <see cref="LeanCollection{T}"/> to <see cref="List{T}"/>. This obviously allocates. For tests only
+        /// </summary>
+        public IReadOnlyList<T> ToList()
+        {
+            var list = new List<T>(Size);
+            foreach (T element in this)
+                list.Add(element);
+            return list;
+        }
+        #endregion
+
+        #region Conversions
+#pragma warning disable CA2225 // Operator overloads have named alternates
+        public static implicit operator LeanCollection<T>(T one) => new LeanCollection<T>(one);
+        public static implicit operator LeanCollection<T>((T, T) pair) => new LeanCollection<T>(pair.Item1, pair.Item2);
+        public static implicit operator LeanCollection<T>((T, T, T) triple) => new LeanCollection<T>(triple.Item1, triple.Item2, triple.Item3);
+        public static implicit operator LeanCollection<T>(T[] span) => new LeanCollection<T>(span);
+
+        public static implicit operator T(LeanCollection<T> one) => one._item1;
+        public static implicit operator (T, T)(LeanCollection<T> pair) => (pair._item1, pair._item2);
+        public static implicit operator (T, T, T)(LeanCollection<T> triple) => (triple._item1, triple._item2, triple._item3);
+        public static implicit operator T[](LeanCollection<T> more) => more._items;
+#pragma warning restore CA2225 // Operator overloads have named alternates
+
+        #endregion
+
+        #region Equality
+        public bool Equals(LeanCollection<T> other)
+        {
+            if (_size != other._size) return false;
+            var equalityComparer = EqualityComparer<T>.Default;
+
+            var enumerator = GetEnumerator();
+            var enumerator2 = other.GetEnumerator();
+            {
+                while (enumerator.MoveNext())
+                    if (!enumerator2.MoveNext() || !equalityComparer.Equals(enumerator.Current, enumerator2.Current))
+                        return false;
+
+                if (enumerator2.MoveNext())
+                    return false;
+            }
+
+            return true;
+        }
+
+        public override bool Equals(object obj) => !(obj is null) && obj is LeanCollection<T> other && Equals(other);
+
+        public override int GetHashCode() => unchecked((int)_size * 397); //size is enough for GetHashCode. After all LeanCollection is not supposed to be used as dictionary key 
+
+        public static bool operator ==(LeanCollection<T> left, LeanCollection<T> right) => left.Equals(right);
+
+        public static bool operator !=(LeanCollection<T> left, LeanCollection<T> right) => !left.Equals(right);
+        #endregion
+
+        public override string ToString() => SpanCollectionSerializer.DefaultInstance.FormatCollection(this);
+
+        public LeanCollection<T> Sort(IComparer<T> comparer = null)
+        {
+            var size = _size;
+            switch (size)
+            {
+                case CollectionSize.Zero:
+                case CollectionSize.One:
+                    return this;
+
+                case CollectionSize.Two:
+                    comparer ??= Comparer<T>.Default;
+                    return comparer.Compare(_item1, _item2) <= 0 ? this : new LeanCollection<T>(_item2, _item1);
+
+                case CollectionSize.Three:
+                    static void Swap(ref T t1, ref T t2)
+                    {
+                        var temp = t2;
+                        t2 = t1;
+                        t1 = temp;
+                    }
+                    comparer ??= Comparer<T>.Default;
+                    if (comparer.Compare(_item1, _item2) <= 0 && comparer.Compare(_item2, _item3) <= 0)
+                        return this;
+                    else
+                    {
+                        T a = _item1, b = _item2, c = _item3;
+
+                        if (comparer.Compare(a, c) > 0)
+                            Swap(ref a, ref c);
+
+                        if (comparer.Compare(a, b) > 0)
+                            Swap(ref a, ref b);
+
+                        if (comparer.Compare(b, c) > 0)
+                            Swap(ref b, ref c);
+                        return new LeanCollection<T>(a, b, c);
+                    }
+                default:
+                    comparer ??= Comparer<T>.Default;
+                    Array.Sort(_items, comparer);
+                    return new LeanCollection<T>(_items);
+            }
+        }
+    }
+}
