d56628f938196b5ae7387777a523fbfed4560d0f
Michał Bryłka <michal.brylka@gmail.com>
2021-03-25 16:36:13 +0100

    MultiDictBench: Change to string keys

--- Benchmarks/MultiKeyDictionaryBench.cs
+++ Benchmarks/MultiKeyDictionaryBench.cs
@@ -1,50 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
 using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
 
 // ReSharper disable CommentTypo
 
 namespace Benchmarks
 {
     [MemoryDiagnoser]
+    [GroupBenchmarksBy(BenchmarkLogicalGroupRule.ByCategory)]
     public class MultiKeyDictionaryBench
     {
         private const int COUNT = 500;
 
         private static readonly Dictionary<string, int> _string = GetStringDict();
-        private static readonly Dictionary<Tuple<int, int>, int> _tuple = GetTupleDict();
-        private static readonly Dictionary<(int, int), int> _valueTuple = GetValueTupleDict();
+        private static readonly Dictionary<Tuple<string, string, string>, int> _tuple = GetTupleDict();
+        private static readonly Dictionary<(string, string, string), int> _valueTuple = GetValueTupleDict();
 
         private static Dictionary<string, int> GetStringDict()
         {
             var dict = new Dictionary<string, int>(COUNT);
             for (int i = 0; i < COUNT; i++)
             {
-                var key = "Client=" + i + "Username" + (i * 10);
+                var key = "Client=" + i + "Username" + (i * 10) + "Token" + (i * 100);
                 dict[key] = i;
             }
 
             return dict;
         }
 
-        private static Dictionary<Tuple<int, int>, int> GetTupleDict()
+        private static Dictionary<Tuple<string, string, string>, int> GetTupleDict()
         {
-            var dict = new Dictionary<Tuple<int, int>, int>(COUNT);
+            var dict = new Dictionary<Tuple<string, string, string>, int>(COUNT);
             for (int i = 0; i < COUNT; i++)
             {
-                var key = new Tuple<int, int>(i, i * 10);
+                var key = new Tuple<string, string, string>(i.ToString(), (i * 10).ToString(), (i * 100).ToString());
                 dict[key] = i;
             }
 
             return dict;
         }
 
-        private static Dictionary<(int, int), int> GetValueTupleDict()
+        private static Dictionary<(string, string, string), int> GetValueTupleDict()
         {
-            var dict = new Dictionary<(int, int), int>(COUNT);
+            var dict = new Dictionary<(string, string, string), int>(COUNT);
             for (int i = 0; i < COUNT; i++)
             {
-                var key = (i, i * 10);
+                var key = (i.ToString(), (i * 10).ToString(), (i * 100).ToString());
                 dict[key] = i;
             }
 
@@ -52,27 +54,17 @@ namespace Benchmarks
         }
 
         [BenchmarkCategory("Build"), Benchmark(Baseline = true)]
-        public int MakeStringDictionary()
-        {
-            var dict = GetStringDict();
-            return dict.Count;
-        }
+        public int MakeStringDictionary() => GetStringDict().Count;
 
         [BenchmarkCategory("Build"), Benchmark]
-        public int MakeTupleDictionary()
-        {
-            var dict = GetTupleDict();
-            return dict.Count;
-        }
+        public int MakeTupleDictionary() => GetTupleDict().Count;
+        
 
         [BenchmarkCategory("Build"), Benchmark]
-        public int MakeValueTupleDictionary()
-        {
-            var dict = GetValueTupleDict();
-            return dict.Count;
-        }
+        public int MakeValueTupleDictionary() => GetValueTupleDict().Count;
 
-        [BenchmarkCategory("Retrieval"), Benchmark]
+
+        [BenchmarkCategory("Retrieval"), Benchmark(Baseline = true)]
         public int FromStringDictionary()
         {
             var dict = _string;
@@ -80,7 +72,7 @@ namespace Benchmarks
 
             for (int i = 0; i < COUNT; i++)
             {
-                var key = "Client=" + i + "Username" + (i * 10);
+                var key = "Client=" + i + "Username" + (i * 10) + "Token" + (i * 100);
                 value = dict[key];
             }
 
@@ -95,7 +87,7 @@ namespace Benchmarks
 
             for (int i = 0; i < COUNT; i++)
             {
-                var key = new Tuple<int, int>(i, i * 10);
+                var key = new Tuple<string, string, string>(i.ToString(), (i * 10).ToString(), (i * 100).ToString());
                 value = dict[key];
             }
 
@@ -110,7 +102,7 @@ namespace Benchmarks
 
             for (int i = 0; i < COUNT; i++)
             {
-                var key = (i, i * 10);
+                var key = (i.ToString(), (i * 10).ToString(), (i * 100).ToString());
                 value = dict[key];
             }
 
