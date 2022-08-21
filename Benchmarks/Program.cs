d74c60a9b524cdd4349cb4616b083cdf6c711d00
Michał Bryłka <michal.brylka@gmail.com>
2022-08-21 14:53:31 +0200

    Remove support for net 5.0

--- Benchmarks/Program.cs
+++ Benchmarks/Program.cs
@@ -1,28 +1,11 @@
-﻿using System.Runtime.CompilerServices;
-using BenchmarkDotNet.Running;
-
-// ReSharper disable CommentTypo
+﻿using BenchmarkDotNet.Running;
 
 namespace Benchmarks
 {
     //dotnet run -c Release --framework net472 -- --runtimes net472 netcoreapp2.2
     internal class Program
     {
-        private static void Main(string[] args)
-        {
-#if DEBUG
-            //new LeanCollectionSum() {Source = new BenchmarkInput<int[]>(new[] {1, 2, 3})}.LeanSumIEnumerable();
-          var tt =  new MultiKeyDictionaryBench();
-
-            var d = new DeconstructablesBench() { N = 10 };
-            d.GlobalSetup();
-            d.Standard();
-            d.Dedicated();
-            d.Convention();
-            d.Deconstructable();
-#endif
+        private static void Main(string[] args) => 
             BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
-        }
     }
-
 }
